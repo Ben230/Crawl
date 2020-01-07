f341e4913b813eb907f6ca50c48edafde4645fc2
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-07 14:26:58 +0000

    inital next setup

---
+++ .next/static/webpack/static/development/pages/about.js.15030e741386bb57e957.hot-update.js
@@ -0,0 +1,40 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/student/Desktop/gitprojects/hello-next/pages/about.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function About() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This is the about page"));
+}
+
+;
+/* harmony default export */ __webpack_exports__["default"] = (About);
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.15030e741386bb57e957.hot-update.js.map
