b0ed6398a4cb8f7c57020d86b6cf366c4a704515
Ed Courage <edcourage@live.com>
2020-01-07 18:03:03 +0000

    jest and react test libary dependencies added

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6c73d2ad6b9d95538ddb";
+/******/ 	var hotCurrentHash = "5313d318171c4f27faa2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
