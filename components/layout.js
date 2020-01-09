f8a268d8640cf0131129b7fe08c706e0858fed9e
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2020-01-09 14:44:25 +0000

    aligned map component to react maps guide

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,6 @@ const Layout = (props) => (
   <div className='root'>
     <Head>
     <title>{props.titleName}</title>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2tah7a3BQ0UGTg668mtESmzbqjyv_AJQ"></script>
     </Head>
         {props.children}
       </div>
