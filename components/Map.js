fe16dd5b816abc355a6a4970ec9295846bad1414
Ed Courage <edcourage@live.com>
2020-01-10 13:14:40 +0000

    markers now load on map

--- components/Map.js
+++ components/Map.js
@@ -8,27 +8,45 @@ import useSWR from 'swr';
 // }
 
 class Map extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {pubs:[]}
+  }
+
 
   componentDidMount(){
     console.log("Method runs!")
     fetch(`http://localhost:3000/api/google-api`)
-    .then((response) => {
-      console.log("Worked!")
-      console.log(response.json());
-    })
+    .then(response => response.json())
+    .then(result => {
+      var pubsArray = result.myJson.results
+      this.setState({pubs:pubsArray})
+
+
+    } );
+
   }
 
    render() {
    const GoogleMapExample = withGoogleMap(props => (
      <GoogleMap
         defaultCenter = { { lat: 51.5176597, lng: -0.072768 } }
-        defaultZoom = { 16 }>
+        defaultZoom = { 14 }>
+        {this.state.pubs.map(pub => (
+          <Marker position={{ lat: pub.geometry.location.lat, lng: pub.geometry.location.lng }} />
+          // <p>lat: {pub.geometry.location.lat} long: {pub.geometry.location.lng}</p>
+
+        ))}
+
         {<Marker position={{ lat: 51.5176597, lng: -0.072768 }} />}
         {<Marker position={{ lat: 51.5176, lng: -0.072768 }} />}
+        {<Marker position={{ lat: 51.5176, lng: -0.072768 }} />}
+
     </GoogleMap>
   ));
    return(
       <div>
+
         <GoogleMapExample
           containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
