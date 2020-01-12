86133f1656401b5de0aa952ba8028eb2c81a84c4
Ed Courage <edcourage@live.com>
2020-01-12 12:16:03 +0000

    map file and class name changes

--- pages/map.js
+++ pages/map.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import Link from 'next/link';
 import Layout from '../components/layout'
-import Map from '../components/Map'
+import MapView from '../components/mapView'
 
 
 
@@ -10,14 +10,14 @@ function fetcher(url) {
   return fetch(url).then(r => r.json());
 }
 
-const MapView = (props) => (
+const PubSearchResults = (props) => (
 
 <Layout titleName={"Map View"}>
   <h1>Map!</h1>
-  <Map/>
+  <MapView/>
   </Layout>
 
 );
 
 
-export default MapView;
+export default PubSearchResults;
