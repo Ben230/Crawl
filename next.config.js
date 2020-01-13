89a395c78fc1dbecf8a7b10690c785702dbf74dc
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-13 13:51:38 +0000

    changed gitignore

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    GOOGLE_MAPS_API_KEY_CLIENT: process.env.GOOGLE_MAPS_API_KEY_CLIENT,
+    GOOGLE_MAPS_API_KEY_SERVER: process.env.GOOGLE_MAPS_API_KEY_SERVER,
+  },
+}
+
+
+// AIzaSyA2tah7a3BQ0UGTg668mtESmzbqjyv_AJQ
