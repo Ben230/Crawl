e9508ccbcb654221fba679cf6e230a439c61cfb6
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-13 12:11:28 +0000

    now debyg

--- components/layout.js
+++ components/layout.js
@@ -7,8 +7,8 @@ const Layout = (props) => (
 
   <div className='root'>
     <Head>
-    // <title>{props.titleName}</title>
-    <title>{process.env.GOOGLE_MAPS_API_KEY_SERVER}</title>
+    <title>{props.titleName}</title>
+    // <title>{process.env.GOOGLE_MAPS_API_KEY_SERVER}</title>
     <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY_CLIENT}&libraries=geometry,drawing,places`}></script>
     </Head>
         {props.children}
