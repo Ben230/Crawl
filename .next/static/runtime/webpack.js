6df395ed7ca25216712131a19ea7e1bccd52adf9
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2020-01-14 16:50:58 +0000

    route page pluged in

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "647e2d46d84d697f52a1";
+/******/ 	var hotCurrentHash = "79c5bdbc3fcef0c7df63";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/directionsMap.js
+++ components/directionsMap.js
@@ -21,18 +21,18 @@ const MapWithADirectionsRenderer = compose(
     componentDidMount(props) {
       const DirectionsService = new google.maps.DirectionsService();
 
-      var origin ={'placeId': this.props.pubsArray.results[0].place_id}
-      var arrayLength = this.props.pubsArray.results.length -1
+      var origin ={'placeId': this.props.pubsArray[0].place_id}
+      var arrayLength = this.props.pubsArray.length -1
       var destination ={
         'placeId':
-        this.props.pubsArray.results[ arrayLength ].place_id
+        this.props.pubsArray[ arrayLength ].place_id
       }
 
       var waypoints = [];
       var i;
       for (i = 1; i < arrayLength; i++) {
         var waypoint = {
-          location: {'placeId': `${this.props.pubsArray.results[i].place_id}`}
+          location: {'placeId': `${this.props.pubsArray[i].place_id}`}
         }
         waypoints.push(waypoint)
       }
