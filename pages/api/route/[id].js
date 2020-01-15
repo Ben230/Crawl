207dd6774953681937090cc7613601ef65736593
Ed Courage <edcourage@live.com>
2020-01-15 18:24:55 +0000

    can now retrieve objects from database

--- pages/api/route/[id].js
+++ pages/api/route/[id].js
@@ -1,6 +1,7 @@
+
 import connectToDb from '../db';
 import mongoose from 'mongoose';
-const PubsSchema = new mongoose.Schema(
+const RoutesSchema = new mongoose.Schema(
   {pubs: [{
             place_id: String,
             name: String,
@@ -9,61 +10,19 @@ const PubsSchema = new mongoose.Schema(
           }
   ]}
 )
-const Pub = mongoose.model("Pub", PubsSchema);
 
-export default (req, res) => {
-  const {
-    query: { id, name },
-    method,
-  } = req
+const Route = mongoose.models.Route || mongoose.model("Route", RoutesSchema);
 
-  switch (method) {
-    case 'GET':
-      // Get data from your database
-      res.status(200).json({ id, name: `User ${id}` })
-      break
-    case 'POST':
-      // Update or create data in your database
-      res.status(200).json({ id, name: name || `User ${id}` })
-      break
-    default:
-      res.setHeader('Allow', ['GET', 'POST'])
-      res.status(405).end(`Method ${method} Not Allowed`)
-  }
+export default async (req, res) => {
+  await connectToDb();
+  
+
+  const route = await Route.find({_id: req.query.id });
+
+
+
+
+
+  res.status(200).json( route )
+
 }
-
-
-
-// export default async (req, res) => {
-//   // await connectToDb();
-//   // const kittySchema = new mongoose.Schema({name: String})
-//
-//   const {
-//      query: { id },
-//    } = req
-//
-//    res.end(`Post: ${id}`)
-//   // var silence = new Kitten({name: "Hello"});
-//   // console.log(silence.name)
-//   //
-//   // silence.save(function (err, silence) {
-//   // if (err) return console.error(err);
-//   // res.send("hello world")
-//
-// });
-//
-//   // const { email, password } = req.body;
-//   // User.findByCredentials(email, password)
-//   //   .then(user => user.generateSessionId())
-//   //   .then(sessionId => {
-//   //     const { name } = user;
-//   //     res
-//   //       .setHeader("Set-Cookie", `sessionId=${sessionId}; HttpOnly; Secure`)
-//   //       .send(`welcome my homie, ${name}`);
-//   //   })
-//   //   .catch(e => {
-//   //     // reject due to wrong email or password
-//   //     res.status(401).send("who are u, i dun know u, go away");
-//   //   });
-//
-// };
