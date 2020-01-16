54108dca9ba1e1c951f6516c6d2264d3a2eb76c9
Ed Courage <edcourage@live.com>
2020-01-16 10:57:26 +0000

    pubs now load around current location

--- pages/api/pubs.js
+++ pages/api/pubs.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-unfetch';
 
 export default (req, res) => {
 
-  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.516967,-0.073133&radius=400&keyword=pub&key=${process.env.GOOGLE_MAPS_API_KEY_SERVER}`)
+  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.lat},${req.query.lng}&radius=400&keyword=pub&key=${process.env.GOOGLE_MAPS_API_KEY_SERVER}`)
   .then((response) => {
     return response.json();
   })
