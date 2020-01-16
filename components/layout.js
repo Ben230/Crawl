40de77f7d6c968d9d84f507a3bc26e0d94e82737
Ed Courage <edcourage@live.com>
2020-01-16 13:41:12 +0000

    nav bar Added

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Navbar from './navbar'
+
 const Layout = (props) => (
 
   <div className='root'>
     <Head>
     <title>{props.titleName}</title>
 
-        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css" >
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css" >
         </link>
 
     <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY_CLIENT}&libraries=geometry,drawing,places`}></script>
     </Head>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
+     <Navbar/>
       {props.children}
       </div>
 )
