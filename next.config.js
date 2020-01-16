b50b46b94b91da08edae54b93252f5a473b30588
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-16 17:49:28 +0000

    attempt fix of now deployment

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
     GOOGLE_MAPS_API_KEY_SERVER: process.env.GOOGLE_MAPS_API_KEY_SERVER,
     DB_LINK: process.env.DB_LINK,
     DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PASSWORD: process.env.DB_PASSWORD
   },
 }
-
-
-// AIzaSyA2tah7a3BQ0UGTg668mtESmzbqjyv_AJQ
