a230e2250ff5c7f145ac801c94fba0ac23fdc3bd
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-16 15:54:31 +0000

    test passed for 2 waypoints

--- functionHelpers/generateClickableURL.js
+++ functionHelpers/generateClickableURL.js
@@ -6,8 +6,21 @@ function generateClickableURL(pubs) {
 
 
   if (pubs.length > 2) {
-    var waypoint_place_id = pubs[1].place_id
-    var waypoint_name = pubs[1].name
+    var waypoint_place_id = ""
+    var waypoint_name = ""
+    for (var i = 1;i<pubs.length-1;i++){
+      waypoint_place_id += pubs[i].place_id + '|'
+      waypoint_name += pubs[i].name + '|'
+
+    }
+    console.log("waypoint_place_id", waypoint_place_id)
+    console.log("waypoint_name", waypoint_name)
+
+
+    waypoint_place_id = waypoint_place_id.slice(0,-1)
+    waypoint_name = waypoint_name.slice(0,-1)
+
+
     var result = `https://www.google.com/maps/dir/?api=1&origin=${origin_name}&origin_place_id=${origin_place_id}&destination=${destination_name}&destination_place_id=${destination_place_id}&waypoints=${waypoint_name}&waypoint_place_ids=${waypoint_place_id}&travelmode=walking`
 
   } else {
