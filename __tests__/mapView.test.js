ee02a1af6b09dfbe05224134d96791fc940f64b4
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-16 13:09:34 +0000

    removing default conditions for map

--- __tests__/mapView.test.js
+++ __tests__/mapView.test.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 import { create } from "react-test-renderer";
-
-
 import MapView from '../components/mapView'
 
 jest.mock('../components/buttonWithObj', () => "hello")
@@ -14,6 +12,8 @@ describe('MapView', function() {
   it ("has defult props lat,longstate and zoom", function() {
     const component = create(<MapView />);
     const instance = component.getInstance();
+    console.log("instance.props")
+    console.log(instance.props)
 
     expect(instance.props.centerLat).toBe(51.516967);
     expect(instance.props.centerLng).toBe(-0.073133);
