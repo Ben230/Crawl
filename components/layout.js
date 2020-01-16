0f2ba0fca02820210f4a42aec7b2d445ec1f2a31
Ed Courage <edcourage@live.com>
2020-01-16 15:08:25 +0000

    image added with some css

--- components/layout.js
+++ components/layout.js
@@ -8,9 +8,9 @@ const Layout = (props) => (
   <div className='root'>
     <Head>
     <title>{props.titleName}</title>
-
-        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css" >
-        </link>
+<link href="/bootstrap.min.css" rel="stylesheet"></link>
+      
+          <link href="/style.css" rel="stylesheet"></link>
 
     <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY_CLIENT}&libraries=geometry,drawing,places`}></script>
     </Head>
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,17 @@ import Layout from '../components/layout'
 const Index = () => (
   <Layout titleName={"Crawl"}>
     <div>
+
+
+    <img id="homePageImage" src="/static/images/crawl-duff-beer.png" height="400" width="400"/>
+
     <Link href="/map">
-      <a>Find Pubs Near Me!</a>
+    <button type="button" className="btn btn-primary btn-lg btn-block">Find Pubs Near Me!</button>
     </Link>
+
+
+
+
     </div>
   </Layout>
 )
