fc0cd196f965ce7f404a393096a2f13b4f311779
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-16 17:42:44 +0000

    added env vars to next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   env: {
     GOOGLE_MAPS_API_KEY_CLIENT: process.env.GOOGLE_MAPS_API_KEY_CLIENT,
     GOOGLE_MAPS_API_KEY_SERVER: process.env.GOOGLE_MAPS_API_KEY_SERVER,
+    DB_LINK: process.env.DB_LINK,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
   },
 }
 
