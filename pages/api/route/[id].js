de6e9f0ea6842c9d64b0f792f211877d0be9eb4e
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-17 11:32:53 +0000

    added scheme bac in

--- pages/api/route/[id].js
+++ pages/api/route/[id].js
@@ -1,7 +1,22 @@
 import connectToDb from '../db';
 import mongoose from 'mongoose';
 
-const Route = mongoose.models.Route
+const RoutesSchema = new mongoose.Schema(
+  {pubs: [{
+            geometry:{
+              location: {lat: Number,lng:Number},
+              viewport:{northeast:{lat: Number,lng:Number},
+                        southwest:{lat: Number,lng:Number}}
+            },
+            place_id: String,
+            name: String,
+            rating: Number,
+            vicinity:String
+          }
+  ]}
+)
+
+const Route = mongoose.models.Route || mongoose.model("Route", RoutesSchema);
 
 export default async (req, res) => {
   await connectToDb();
