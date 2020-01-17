764a6139f4e804e6ec8ec1bbfd2c2a10adddca8c
Ben Stuart <benarthurstuart@hotmail.com>
2020-01-17 11:14:12 +0000

    hardcode in db setup

--- pages/api/db.js
+++ pages/api/db.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 export default async () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
-  await mongoose.connect(process.env.DB_LINK,
-  { useNewUrlParser: true, user: process.env.DB_USER, pass: process.env.DB_PASSWORD });
+  await mongoose.connect("mongodb+srv://cluster0-jygbk.mongodb.net/crawl",
+  { useNewUrlParser: true, user: "Crawl_1", pass: "crawl" });
 };
